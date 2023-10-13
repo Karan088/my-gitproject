7cffb45b641f161ff00743d5e53ac42ad1fad515
Karan088 <karansubramaniam8@gmail.com>
2023-10-13 15:48:19 +0530

    Code improvement

--- 206051G-mycode.py
+++ 206051G-mycode.py
@@ -1,6 +1,9 @@
-number = int(input("Enter a number:"))
+try:
+    number = int(input("Enter a number:"))
 
-if number % 2 == 0:
-    print(number, "is even.")
-else:
-    print(number, "is odd.")
+    if number % 2 == 0:
+        print(number, "is even.")
+    else:
+        print(number, "is odd.")
+except ValueError:
+    print("Invalid input. Please enter a valid integer.")

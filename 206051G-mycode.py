13b807ac4c78eeae8b60097f625087a43274d0c1
Karan088 <karansubramaniam8@gmail.com>
2023-10-13 16:20:31 +0530

    Code simplified

--- 206051G-mycode.py
+++ 206051G-mycode.py
@@ -1,11 +1,26 @@
-print("Even or Odd Number Checker :Try Now")
+print("Even or Odd, Prime, and Circle Perimeter Checker")
 
 try:
-    number = int(input("Enter a number:"))
+    choice = int(input("Choose an option:\n1. Even or Odd\n2. Prime or Not\n3. Circle Perimeter\nEnter the corresponding number: "))
 
-    if number % 2 == 0:
-        print(number, "is even.")
+    if choice == 1:
+        number = int(input("Enter a number:"))
+        result = "even" if number % 2 == 0 else "odd"
+        print(f"{number} is {result}.")
+    elif choice == 2:
+        number = int(input("Enter a number:"))
+        if number > 1:
+            is_prime = all(number % i != 0 for i in range(2, int(number**0.5) + 1))
+            prime_status = "a prime" if is_prime else "not a prime"
+            print(f"{number} is {prime_status} number.")
+        else:
+            print(f"{number} is not a prime number.")
+    elif choice == 3:
+        radius = float(input("Enter the radius of the circle:"))
+        perimeter = 2 * 3.14159 * radius
+        print(f"The perimeter of the circle with radius {radius} is {perimeter:.2f}.")
     else:
-        print(number, "is odd.")
+        print("Invalid choice. Please choose 1, 2, or 3.")
 except ValueError:
-    print("Invalid input. Please enter a valid integer.")
+    print("Invalid input. Try Again later.")
+

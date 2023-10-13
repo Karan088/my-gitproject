59e2d4e97bfd03dc189a0568a15e7dad7a4c44fb
Karan088 <karansubramaniam8@gmail.com>
2023-10-13 16:03:26 +0530

    New feature

--- 206051G-mycode.py
+++ 206051G-mycode.py
@@ -1,4 +1,4 @@
-print("Even or Odd Number Checker :Try Now")
+print("Even or Odd & Prime Number Checker")
 
 try:
     number = int(input("Enter a number:"))
@@ -7,5 +7,16 @@ try:
         print(number, "is even.")
     else:
         print(number, "is odd.")
+
+    if number > 1:
+        for i in range(2, int(number ** 0.5) + 1):
+            if number % i == 0:
+                print(number, "is not a prime number.")
+                break
+        else:
+            print(number, "is a prime number.")
+    else:
+        print(number, "is not a prime number.")
+
 except ValueError:
     print("Invalid input. Please enter a valid integer.")
